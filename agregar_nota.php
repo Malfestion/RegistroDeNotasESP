05e487cb318a9d9b7e4cc6e25461a20f1cb9e918
Alejandro DL <aleduar98@gmail.com>
2023-08-28 08:26:50 -0600

    Funcion de agregar notas
    Se agrega una pagina para agregar notas a la BD

--- agregar_nota.php
+++ agregar_nota.php
@@ -2,9 +2,108 @@
 // (c) 2023 Alejandro Duarte Lobo
 // This code is licensed under BSD 3-Clause License (see LICENSE for details)
 
-date_default_timezone_set("America/Costa_Rica");
-echo "Fecha y hora " . date("Y/m/d, h:i:sa");
-for ($i = 0; $i < 10000; $i++) {
-    echo ("<p>" . $i . "- PENDIENTE:-Menu de opciones homologo al menu de edicion sin los valores predeterminados.</p>");
-}
-?>
+//se inicia sesion y se verifica si el rol es admin
+session_start();
+if (isset($_SESSION['username']) && isset($_SESSION['id']) && $_SESSION['role'] == 'admin') {
+        include "php/db_conn.php";
+
+        $sql = "SELECT * FROM notas
+        JOIN estudiante ON (estudiante.id = notas.id_estudiante)
+        JOIN area ON (area.id = notas.id_area) 
+        JOIN profesor ON (profesor.id = notas.id_profesor)
+        JOIN nivel ON (nivel.id = notas.id_nivel)    
+        WHERE id_nota='$id' ";
+        $query = mysqli_query($conn, $sql);
+        $row = $row = mysqli_fetch_array($query);
+        ?>
+
+        <!DOCTYPE html>
+        <html lang="es">
+
+        <?php
+        include "layout/head.php";
+        ?>
+
+        <body>
+                <?php
+                include "layout/header.php";
+                ?>
+                <div class="container" style="margin-top: 80px; margin-bottom: 80px;">
+                        <form action="php/insert_nota.php" method="POST">
+                                <h1>Agregar Registro</h1>
+                                <div class="mb-3  p-3">
+                                        <label for="estudiante" class="form-label">Estudiante</label>
+                                        <select class="form-select select2-single" id="estudiante" name="estudiante">
+                                                <?php
+                                                $res_estudiante = mysqli_query($conn, "SELECT * FROM estudiante");
+                                                while ($row_estudiante = mysqli_fetch_array($res_estudiante)): ?>
+                                                        <option value="<?= $row_estudiante['id'] ?>"><?= $row_estudiante['id'] ?>
+                                                        </option>
+                                                <?php endwhile; ?>
+                                        </select>
+                                </div>
+
+                                <div class="mb-3  p-3">
+                                        <label for="area" class="form-label">Area</label>
+                                        <select class="form-select select2-single" id="area" name="area">
+                                                </option>
+                                                <?php
+                                                $res_area = mysqli_query($conn, "SELECT * FROM area");
+                                                while ($row_area = mysqli_fetch_array($res_area)): ?>
+                                                        <option value="<?= $row_area['id'] ?>"><?= $row_area['nombre_area'] ?></option>
+                                                <?php endwhile; ?>
+                                        </select>
+                                </div>
+                                <div class="mb-3  p-3">
+                                        <label for="profesor" class="form-label">Profesor</label>
+                                        <select class="form-select select2-single" id="profesor" name="profesor">
+                                                <?php
+                                                $res_profesor = mysqli_query($conn, "SELECT * FROM profesor");
+                                                while ($row_profesor = mysqli_fetch_array($res_profesor)): ?>
+                                                        <option value="<?= $row_profesor['id'] ?>"><?= $row_profesor['nombre_profesor'] ?></option>
+                                                <?php endwhile; ?>
+                                        </select>
+                                </div>
+                                <div class="mb-3  p-3">
+                                        <label for="nivel" class="form-label">Nivel</label>
+                                        <select class="form-select select2-single" id="nivel" name="nivel">
+                                                <?php
+                                                $res_nivel = mysqli_query($conn, "SELECT * FROM nivel");
+                                                while ($row_nivel = mysqli_fetch_array($res_nivel)): ?>
+                                                        <option value="<?= $row_nivel['id'] ?>"><?= $row_nivel['nombre_nivel'] ?>
+                                                        </option>
+                                                <?php endwhile; ?>
+                                        </select>
+                                </div>
+                                <div class="mb-3  p-3">
+                                        <label for="grupo" class="form-label">Grupo</label>
+                                        <input type="text" class="form-control" id="grupo" name="grupo" placeholder="Nombre del grupo" required="required">
+                                </div>
+                                <div class="mb-3  p-3">
+                                        <label for="periodo" class="form-label">Periodo Lectivo</label>
+                                        <input type="text" class="form-control" id="periodo" name="periodo" placeholder="Periodo Lectivo" required="required">
+                                </div>
+                                <div class="mb-3  p-3">
+                                        <label for="tel" class="form-label">Nota</label>
+                                        <input type="text" class="form-control" id="nota" name="nota" placeholder="Nota" required="required"> 
+                                </div>
+                                <input type="submit" class="btn btn-primary" style="margin-left: 15px;"
+                                        value="Agregar">
+
+                        </form>
+                </div>
+                <?php
+                include "layout/footer.php";
+                ?>
+                <script type="text/javascript">
+                        $(document).ready(function () {
+                                $('.select2-single').select2();
+                        });
+                </script>
+
+        </body>
+
+        </html>
+<?php } else {
+        header("Location: login.php");
+} ?>
---
+++ php/insert_nota.php
@@ -0,0 +1,24 @@
+<?php
+// (c) 2023 Alejandro Duarte Lobo
+// This code is licensed under BSD 3-Clause License (see LICENSE for details)
+
+session_start();
+if (isset($_SESSION['username']) && isset($_SESSION['id']) && $_SESSION['role'] == 'admin') {
+    include "db_conn.php";
+
+    $id_estudiante = $_POST['estudiante'];
+    $id_area = $_POST['area'];
+    $id_profesor = $_POST['profesor'];
+    $id_nivel = $_POST['nivel'];
+    $nombre_grupo= $_POST['grupo'];
+    $periodo = $_POST['periodo'];
+    $nota = $_POST['nota'];
+
+    $sql = "INSERT INTO notas (id_estudiante, id_area, id_profesor, id_nivel, nombre_grupo, periodo, nota) VALUES('$id_estudiante','$id_area','$id_profesor','$id_nivel','$nombre_grupo','$periodo','$nota')";
+    $query = mysqli_query($conn, $sql);
+    if ($query) {
+        Header("Location: ../estudiantes.php");
+    }
+    ;
+}
+?>
