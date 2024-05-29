607fd3a2e145f06db2b23d5460a500dc51984e50
Alejandro DL <aleduar98@gmail.com>
2024-05-29 16:36:52 -0600

    correccion provisional del inv sort
    Al dar el 3er sort desordena la tabla pero no causa error. Se necesita analizar mejor la causa de los problemas al dar click siempre y unicamente por 3era vez.(wtf javascript)

--- php/ajax-grid-data-inventario.php
+++ php/ajax-grid-data-inventario.php
@@ -45,7 +45,33 @@ $query = mysqli_query($conn, $sql);
 $totalFiltered = mysqli_num_rows($query);
 
 // Add sorting functionality
-$sql .= " ORDER BY " . $col[$request['order'][0]['column']] . " " . $request['order'][0]['dir'] . " LIMIT " . $request['start'] . ", " . $request['length'];
+// error handle: provisional
+$order_sql = '';
+if (!empty($request['order'])) {
+    $order_sql = " ORDER BY ";
+    $order_clauses = [];
+    foreach ($request['order'] as $order) {
+        $column_idx = $order['column'];
+        $column_dir = $order['dir'];
+        if (isset($col[$column_idx]) && in_array($column_dir, ['asc', 'desc'])) {
+            $order_clauses[] = $col[$column_idx] . " " . $column_dir;
+        }
+    }
+    if (!empty($order_clauses)) {
+        $order_sql .= implode(", ", $order_clauses);
+    } else {
+        $order_sql = ''; // If no valid order clauses, reset order_sql
+    }
+}
+
+// Add limit for pagination
+$start = $request['start'];
+$length = $request['length'];
+$limit_sql = " LIMIT " . $start . ", " . $length;
+
+$sql .= $order_sql . $limit_sql;
+
+$query = mysqli_query($conn, $sql) or die("ajax-grid-data-inventario.php: get orders. SQL: " . $sql);
 
 // Fetch data
 $query = mysqli_query($conn, $sql) or die("ajax-grid-data-inventario.php: get orders"."sql: ".$sql);
